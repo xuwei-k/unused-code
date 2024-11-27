8be6c6f07d54b97ec6552b07e0cc08cf5c622e2e
xuwei-k <6b656e6a69@gmail.com>
2024-11-27 19:54:18 +0900

    add enum test

--- plugin/src/sbt-test/t1/scala3/a1/Hello.scala
+++ plugin/src/sbt-test/t1/scala3/a1/Hello.scala
@@ -3,9 +3,22 @@ package hello
 // https://docs.scala-lang.org/scala3/book/methods-main-methods.html
 
 object Hello1 {
-  @main def x1(args: String*): Unit = println("hello " + args.mkString(", "))
+  @main def x1(args: String*): Unit = {
+    println(ExampleEnum1.EnumCaseA)
+    println(ExampleEnum2.EnumCaseY)
+    println("hello " + args.mkString(", "))
+  }
 }
 
 object Hello2 {
   @main def x2(a1: Int = 3): Int = a1 + 2
+}
+
+enum ExampleEnum1 {
+  case EnumCaseA
+  case EnumCaseB
+}
+
+enum ExampleEnum2 {
+  case EnumCaseX, EnumCaseY
 }
