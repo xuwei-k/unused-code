b79644d37ab3aff64f97deb20f51716c817687fa
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2025-09-30 08:55:38 +0000

    Update scala-library to 2.13.17

--- plugin/src/main/scala/unused_code/UnusedCodePlugin.scala
+++ plugin/src/main/scala/unused_code/UnusedCodePlugin.scala
@@ -85,7 +85,7 @@ object UnusedCodePlugin extends AutoPlugin {
     val buildSbt =
       s"""|name := "tmp-unused-code"
           |logLevel := Level.Warn
-          |scalaVersion := "2.13.16"
+          |scalaVersion := "2.13.17"
           |libraryDependencies ++= Seq(
           |  "com.github.xuwei-k" %% "unused-code-scalafix" % "${UnusedCodeBuildInfo.version}"
           |)
